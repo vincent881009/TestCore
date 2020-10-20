38c53b84372deac5f87ede47e74e171c91ef9a21
Akrios yu <gacktsong@hotmail.com>
2020-10-20 22:59:39 +0800

    app

---
+++ CoreTest.App/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CoreTest.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
